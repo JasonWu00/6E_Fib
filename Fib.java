a359fe9515c688ccc9528915aceda192d0686eed
Jason Wu <zwu00@stuy.edu>
2019-05-13 22:07:42 -0400

    done.

--- Fib.java
+++ Fib.java
@@ -1,58 +1,81 @@
-/** 
+/**
   Calculate Fibonacci number n
   3 implementations
   @precondition: n >= 1
-  
+
   Use type "long" instead of "int", because rabbits.
  */
 
 public class Fib {
-    /** 
+    /**
       @return the nth Fibonacci number
               by implementing the recurrence relation
      */
-    public ?? long fib_recurrence( int n) {
-        return -2; // invalid Fibonacci number
+    public static long fib_recurrence( int n) {
+        if (n == 1 || n == 2) {//binary decision
+          return 1;//basecase solution
+        }
+        else {
+          return fib_recurrence(n-1) + fib_recurrence(n-2);//recursive abstraction
+          //includes combining and leftover steps here.
+        }
     }
-     /* These are class / instance methods because...
+     /* These are class methods because the fib_recurrence method will
+     return the same value for the same input regardless of which instance
+     of the class it is invoked in, so it would be better to make it a class
+     method.
     */
 
 
-    /** 
+    /**
       @return the nth Fibonacci number
               calculated via the 8th-grade algorithm
      */
-    public ?? long fib_grade8( int n) {
-        return -2; // invalid Fibonacci number
+    public static long fib_grade8( int n) {
+        long lowerPair = 1;
+        long higherPair = 1;
+        if (n == 1 || n == 2) return 1;
+        else {
+          for (int index = 3; index < n; index++) {
+            long oldHigherPair = higherPair;
+            higherPair += lowerPair;
+            lowerPair = oldHigherPair;
+          }
+        }
+        return higherPair + lowerPair;
     }
     /* Time complexity:
        Consider the size of the problem to be ...
-       
+
        As the proxy for the time required, count...
-       
+
        Then cost of the the recurrence algorithm
-       grows ?? 
+       grows ??
        as the size of the problem increases,
        because ??
      */
 
 
-    /** 
+    /**
       @return the nth Fibonacci number
               calculated via Binet's formula.
               Type is double so that this exercise can
               ignore rounding issues.
      */
-    public ?? double fib_Binet( int n) {
-        return -2; // invalid Fibonacci number
+    public static long fib_Binet( int n) {
+        //multiple variables used here to prevent confusion on my part
+        double firstVarOnNumerator = Math.pow((1 + Math.sqrt(5)) /2, n);
+        double secondVarOnNumerator = Math.pow((1 - Math.sqrt(5)) /2, n);
+        double output = (firstVarOnNumerator - secondVarOnNumerator) / Math.sqrt(5);
+        return (long) output; //temp until code done
     }
     /* Time complexity:
        Consider the size of the problem to be ...
-       
+
        As the proxy for the time required, count...
-       
+
        Then cost of the the recurrence algorithm
-       grows ?? 
+       grows ??
        as the size of the problem increases,
        because ??
      */
--- UserOfFib.java
+++ UserOfFib.java
@@ -1,13 +1,13 @@
-/** 
+/**
   test Fib methods, in response to command line input
-  
+
   java UserOfFib @method @index
-  where @method is one of 
+  where @method is one of
       recurrence
       grade8
       Binet
    and @index is the index of the desired Fibonacci number
-   
+
    For example
           java UserOfFib recurrence 7
     should produce 13
@@ -15,20 +15,29 @@
 
 public class UserOfFib {
     public static void main(String[] commandLine) {
-        
+
         String method = commandLine[0];
-        int seek = Integer.valueOf( commandLine[1]);  
+        int seek = Integer.valueOf( commandLine[1]);
 
         System.out.println( "using method " + method
                           + " seeking F_" + seek);
 
         if(  method.equals( "recurrence"))
-            System.out.println( "Fib " + Fib.fib_recurrence( seek));
+            System.out.println( "Fib " + callRecurrence( seek));
         else if( method.equals( "grade8" ))
-            System.out.println( "Fib " + Fib.fib_grade8( seek));
+            System.out.println( "Fib " + callGrade8( seek));
         else if( method.equals( "Binet"))
-            System.out.println( "Fib " + Fib.fib_Binet( seek));
+            System.out.println( "Fib " + callBinet( seek));
 
         else System.out.println(  "invalid method on the command line");
+    }
+    public static long callRecurrence(int seek) {
+      return Fib.fib_recurrence(seek);
+    }
+    public static long callGrade8(int seek) {
+      return Fib.fib_grade8(seek);
+    }
+    public static double callBinet(int seek) {
+      return Fib.fib_Binet(seek);
     }
 }
